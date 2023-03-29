4730bf519ae107aa75d2661548667bb2826c5f36
KABANYANA <joyeusekabanyana@gmail.com>
2023-03-29 23:21:24 +0300

    new

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,14 @@ fun main() {
 
 println( evenIndex(listOf("Joyce","Shila", "lekishon", "Cynthia", "Joy", "Landy", "Floris", "Nancy", "Arnaud", "Ishimwe")))
 println(peopleHeight(listOf(12.4,34.7,23.5,15.8,40.2)))
-var carry=Car("RAB2333",34.6)
-    var carry1=Car("RAB23",98.6)
+
+    println(mill())
 
 
 }
+
+
+
 //    Given a list of 10 strings, write a function that returns a list of the strings
 //    at even indices i.e index 2,4,6 etc
 
@@ -24,30 +27,26 @@ fun peopleHeight(heights:List<Double>):List<Double>{
 
 //Given a list of Person objects, each with the attributes, name, age,
 //height and weight. Sort the list in order of descending age
- class Student(var name:String, var age:Int, var height:Double, var weight:Double) {
-  open fun studentList(st1: Student,st2:Student,st3:Student,st4:Student) {
+ data class Student(var name:String, var age:Int, var height:Double, var weight:Double) {
+    fun studentList() {
+        var st1 = Student("Nancy", 22, 1.78, 70.3)
+        var st2 = Student("Mwamkinga", 23, 1.50, 70.0)
 
- var stList = listOf(st1, st2, st3, st4)
-    var sorted = stList.sortedBy { student -> student.age }
-   println(sorted)
-  }
 
+        var stList = listOf(st1, st2)
+        var sorted = stList.sortedBy { student -> student.age }
+        println(sorted)
     }
-
-
-
-//    var st6 = Student("Nancy", 22, 1.78,70.3)
-//    var st7 = Student("Mwamkinga", 23, 1.50,70.0)
-//    var newList =Student.add(st6)
-//    var newList1 =Student.add(st7)
+}
 
 
 
 
-//}
 
-//Given a list similar to the one above, write a function in which you will
-//create 2 more people objects and add them to the list at one go.
+
+
+
+
 
 
 
@@ -55,12 +54,25 @@ fun peopleHeight(heights:List<Double>):List<Double>{
 //Write a function that takes in a list of Car objects each with a
 //registration and mileage attribute and returns the average mileage of
 //all the vehicles in the list.
-class Car(var registration:String,var mileage:Double) {
-    fun averageMileage(cars: List<Car>): Double {
-       var totalMileage =0.0
-        for (car in cars) {
-            totalMileage += car.mileage
-        }
-        return totalMileage/cars.size
+data class Car(var registration:String,var mileage:Double)
+fun mill():Double {
+    var c1 = Car("ABC123", 100.00)
+    var c2 = Car("DEF456", 200.00)
+    var c3 = Car("GHI789", 150.00)
+
+    var cars = listOf(c1, c2, c3)
+
+    var sum = 0.0
+    for (c in cars) {
+        sum += c.mileage
+
+
     }
+    val avg= sum / cars.size
+    return avg
 }
+
+
+
+
+
