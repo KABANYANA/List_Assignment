707a20aa155bddacce557e02e39b93812b7ea2c2
KABANYANA <joyeusekabanyana@gmail.com>
2023-03-29 17:12:23 +0300

    new

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,9 @@ fun main() {
 
 println( evenIndex(listOf("Joyce","Shila", "lekishon", "Cynthia", "Joy", "Landy", "Floris", "Nancy", "Arnaud", "Ishimwe")))
 println(peopleHeight(listOf(12.4,34.7,23.5,15.8,40.2)))
+var carry=Car("RAB2333",34.6)
+    var carry1=Car("RAB23",98.6)
+
 
 }
 //    Given a list of 10 strings, write a function that returns a list of the strings
@@ -21,14 +24,14 @@ fun peopleHeight(heights:List<Double>):List<Double>{
 
 //Given a list of Person objects, each with the attributes, name, age,
 //height and weight. Sort the list in order of descending age
-data class Student(var name:String, var age:Int, var height:Double, var weight:Double) {
+ class Student(var name:String, var age:Int, var height:Double, var weight:Double) {
   open fun studentList(st1: Student,st2:Student,st3:Student,st4:Student) {
 
  var stList = listOf(st1, st2, st3, st4)
     var sorted = stList.sortedBy { student -> student.age }
    println(sorted)
   }
-override
+
     }
 
 
@@ -52,3 +55,12 @@ override
 //Write a function that takes in a list of Car objects each with a
 //registration and mileage attribute and returns the average mileage of
 //all the vehicles in the list.
+class Car(var registration:String,var mileage:Double) {
+    fun averageMileage(cars: List<Car>): Double {
+       var totalMileage =0.0
+        for (car in cars) {
+            totalMileage += car.mileage
+        }
+        return totalMileage/cars.size
+    }
+}
